392e80bf97421798996bec36a6d8f84fc9ed274d
jonathonCali <94307128+jonathonCali@users.noreply.github.com>
2024-01-13 00:41:20 -0700

    Add files via upload

---
+++ selecting multiple elements lesson/index.js
@@ -0,0 +1,6 @@
+// var things = document.getElementsByClassName("things")
+// console.log(things[0])
+var things = document.getElementsByClassName("things")
+for (var i = 0; i < things.length; i++) {
+    console.log(things[i])
+}
